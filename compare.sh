ba77c2738a6ab6d83ff190b459135e6757f823e2
kusakata <shohei@kusakata.com>
2018-12-06 23:32:23 +0900

    fix source url in compare.sh

--- compare.sh
+++ compare.sh
@@ -4,8 +4,9 @@ then
     file=`cat ${json} | jq -r .man`
     pkg=`cat ${json} | jq -r .package`
     ver=`cat ${json} | jq -r .version`
+    firstletter=`echo ${pkg} | cut -c 1-1`
     mkdir -p /tmp/man-page-compare
-    wget https://archive.archlinux.org/packages/.all/${pkg}-${ver}-x86_64.pkg.tar.xz -O /tmp/man-page-compare/original.tar.xz
+    wget https://archive.archlinux.org/packages/${firstletter}/${pkg}/${pkg}-${ver}-any.pkg.tar.xz -O /tmp/man-page-compare/original.tar.xz
     tar xvf /tmp/man-page-compare/original.tar.xz -C /tmp/man-page-compare
     meld /tmp/man-page-compare${file} ${file}
 else
